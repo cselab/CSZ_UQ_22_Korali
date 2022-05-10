973f0dfb0a5893be4a65c2be6925706c9fde7a8b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-05-10 10:31:48 -0400

    add 1_rosenbrock

---
+++ 1_rosenbrock/main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import korali
+import numpy as np
+
+
+def rosenbrock(x, y):
+    """
+    The 2D Rosenbrock function
+    https://en.wikipedia.org/wiki/Rosenbrock_function
+    """
+    a = 1
+    b = 100
+    return (a - x)**2 + b * (y - x**2)**2
+
+
+if __name__ == '__main__':
+
+    # The optimization problem is configures into a
+    # korali Experiment object
+    e = korali.Experiment()
+
+    # Korali requires a specific interface for the function to maximize
+    def objective_function(ksample):
+        x, y = ksample["Parameters"]
+        ksample["F(x)"] = -rosenbrock(x, y)
+
+    e["Problem"]["Type"] = "Optimization"
+    e["Problem"]["Objective Function"] = objective_function
+
+    # Defining the problem's variables.
+    e["Variables"][0]["Name"] = "x"
+    e["Variables"][0]["Initial Value"] = 0.0
+    e["Variables"][0]["Initial Standard Deviation"] = 1.0
+
+    e["Variables"][1]["Name"] = "y"
+    e["Variables"][1]["Initial Value"] = 0.0
+    e["Variables"][1]["Initial Standard Deviation"] = 1.0
+
+    # Configuring CMA-ES parameters
+    e["Solver"]["Type"] = "Optimizer/CMAES"
+    e["Solver"]["Population Size"] = 16
+    e["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 1e-8
+    e["Solver"]["Termination Criteria"]["Max Generations"] = 100
+
+    # Experiments need a Korali Engine object to be executed
+    k = korali.Engine()
+
+    # Run the optimization
+    k.run(e)
