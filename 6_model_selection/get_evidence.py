5531e9b9224bb9e7ba5fc7f79797b74618afc258
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-05-11 12:02:36 -0400

    cleanup

--- 6_model_selection/get_evidence.py
+++ 6_model_selection/get_evidence.py
@@ -4,21 +4,6 @@ import json
 import numpy as np
 import os
 
-def get_get_evidence(filename):
-    a = []
-    b = []
-    sigma = []
-
-    with open(filename) as f:
-        doc = json.load(f)
-
-        for sample in doc["Results"]["Posterior Sample Database"]:
-            a.append(sample[0])
-            b.append(sample[1])
-            sigma.append(sample[2])
-
-    return np.array(a), np.array(b), np.array(sigma)
-
 if __name__ == '__main__':
 
     log_evidences = []
