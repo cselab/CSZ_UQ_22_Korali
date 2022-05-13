309ffc37c4a725b98948c4605b658f6cf69c0aa8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-05-13 13:36:43 -0400

    conduits: add distributed case

---
+++ 4_conduits/run_distributed.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import argparse
+import korali
+import numpy as np
+import pandas as pd
+import time
+
+from mpi4py import MPI
+
+def load_data(filename: str):
+    df = pd.read_csv(filename)
+    x = df['x'].to_numpy()
+    y = df['y'].to_numpy()
+    return x, y
+
+if __name__ == '__main__':
+
+    e = korali.Experiment()
+
+    x, y = load_data('data.csv')
+
+    def model(ksample):
+        a, b, sigma = ksample["Parameters"]
+        y_pred = a * x + b
+        ksample["Reference Evaluations"] = y_pred.tolist()
+        ksample["Standard Deviation"] = len(y_pred) * [sigma]
+        time.sleep(0.1) # to mimick a more expensive model
+
+    e["Random Seed"] = 0xC0FFEE
+
+    e["Problem"]["Type"] = "Bayesian/Reference"
+    e["Problem"]["Likelihood Model"] = "Normal"
+    e["Problem"]["Reference Data"] = y.tolist()
+    e["Problem"]["Computational Model"] = model
+
+    e["Variables"][0]["Name"] = "a"
+    e["Variables"][0]["Prior Distribution"] = "Prior a"
+
+    e["Variables"][1]["Name"] = "b"
+    e["Variables"][1]["Prior Distribution"] = "Prior b"
+
+    e["Variables"][2]["Name"] = "sigma"
+    e["Variables"][2]["Prior Distribution"] = "Prior sigma"
+
+    e["Distributions"][0]["Name"] = "Prior a"
+    e["Distributions"][0]["Type"] = "Univariate/Uniform"
+    e["Distributions"][0]["Minimum"] = 1.0
+    e["Distributions"][0]["Maximum"] = 5.0
+
+    e["Distributions"][1]["Name"] = "Prior b"
+    e["Distributions"][1]["Type"] = "Univariate/Uniform"
+    e["Distributions"][1]["Minimum"] = -5.0
+    e["Distributions"][1]["Maximum"] = +1.0
+
+    e["Distributions"][2]["Name"] = "Prior sigma"
+    e["Distributions"][2]["Type"] = "Univariate/Uniform"
+    e["Distributions"][2]["Minimum"] = 0.0
+    e["Distributions"][2]["Maximum"] = 2.0
+
+    # Configuring TMCMC parameters
+    e["Solver"]["Type"] = "Sampler/TMCMC"
+    e["Solver"]["Population Size"] = 100
+
+    # Experiments need a Korali Engine object to be executed
+    k = korali.Engine()
+
+    # Setup distributed conduit
+    k.setMPIComm(MPI.COMM_WORLD) # pass the communicator to korali
+    k["Conduit"]["Type"] = "Distributed"
+    k["Conduit"]["Ranks Per Worker"] = 1
+
+    # Run the optimization
+    k.run(e)
--- 4_conduits/run_speedup.sh
+++ 4_conduits/run_speedup.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
-f="timings_concurrent.csv"
-echo "n,time" > $f
+concurrent()
+{
+    f="timings_concurrent.csv"
+    echo "n,time" > $f
 
-for n in `seq 4`; do
-    echo "Running with $n core(s)..."
-    time=`./run_concurrent.py --num-cores $n | grep "Elapsed Time:" | grep -Eo '[0-9]+([.][0-9]+)?'`
-    echo "${n},${time}" >> $f
-done
+    for n in `seq 4`; do
+	echo "Running with $n core(s)..."
+	time=`./run_concurrent.py --num-cores $n | grep "Elapsed Time:" | grep -Eo '[0-9]+([.][0-9]+)?'`
+	echo "${n},${time}" >> $f
+    done
+}
+
+distributed()
+{
+    f="timings_distributed.csv"
+    echo "n,time" > $f
+
+    for n in `seq 4`; do
+	echo "Running with $n core(s)..."
+	time=`mpirun --oversubscribe -n $(($n+1)) ./run_distributed.py | grep "Elapsed Time:" | grep -Eo '[0-9]+([.][0-9]+)?'`
+	echo "${n},${time}" >> $f
+    done
+}
+
+distributed
